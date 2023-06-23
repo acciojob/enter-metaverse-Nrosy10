f4e60c53a5610dadd5dc523cb6985bf21a9c99c4
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-23 10:58:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let para=document.getElementById("status");
 let bttn= document.getElementById("enterBtn");
 bttn.addEventListener("click",function(){
 	para.innerText="Entered Metaverse";
+	let head=document.createElement('h1');
+	head.innerText=para.innerText;
+	para.replaceWith(head);
 });
